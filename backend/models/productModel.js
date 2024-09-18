b609d6f6d3adaaf19f09226565e798e4dacc2de5
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-09-18 15:50:14 -0500

    product model added

---
+++ backend/models/productModel.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const {
+  color,
+} = require("../../frontend/src/Customers/Components/Product/FilterData");
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  discountedPrice: {
+    type: Number,
+  },
+  discountedPersent: {
+    type: Number,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  sizes: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ratings",
+  },
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "reviews",
+    },
+  ],
+  numRatings: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Product = mongoose.model("products", productSchema);
+module.exports = Product;
