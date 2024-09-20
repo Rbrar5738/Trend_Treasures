32a1bdea4aea9dc992cd1eaf0f279be356a1f350
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-09-20 10:34:58 -0500

    User controllers added

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,9 @@ const app = express();
 require("dotenv").config();
 const { mongoConnect } = require("./mongoConnect");
 const cors = require("cors");
+const authRoute = require("../routes/authRoute");
+const userRoute = require("../routes/userRoute");
+
 app.use(cors());
 
 app.use(express.json());
@@ -21,6 +24,9 @@ mongoConnect(process.env.MONGODB_URI, {
 app.get("/", (req, res) => {
   res.status(200).send(`Welcome to Backend`);
 });
+
+app.use("/auth", authRoute);
+app.use("/users", userRoute);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
