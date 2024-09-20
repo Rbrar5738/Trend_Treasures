297ffe180704cb7e1e154db80b2ece1b3feb959a
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-09-20 10:15:20 -0500

    Login controller added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const userService = require("../services/userService");
 const jwtProvider = require("../config/jwtProvider");
+const bcrypt = require("bcrypt");
 
 //Register a new user
 const register = async (req, res) => {
@@ -7,26 +8,36 @@ const register = async (req, res) => {
     const user = await userService.createUser(req.body);
     const jwt = jwtProvider.generateToken(user._id);
     // await cartService.ccreateCart(user);
-    res.status(200).send({ jwt, message: "registered successfully" });
+    return res.status(200).send({ jwt, message: "registered successfully" });
   } catch (err) {
-    res.status(500).send({ message: err.message });
+    return res.status(500).send({ message: err.message });
   }
 };
 
 //Login user
 const login = async (req, res) => {
   try {
-
-    const {email, password}=req.body;
+    const { email, password } = req.body;
     const user = await userService.getUserByEmail(email);
-    if (!user) {  
-      return res.status(404).send({ message: "User not found" });
+    if (!user) {
+      return res
+        .status(404)
+        .send({ message: "User not found with email:", email });
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).send({ message: "Invalid Password" });
+    }
+    const jwt = jwtProvider.generateToken(user._id);
+
+    return res.status(200).send({ jwt, message: "Login Success" });
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
-}
+};
 
 module.exports = {
   register,
+  login,
 };
