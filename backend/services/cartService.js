145939e02b549f8e327ab266f24747c3352dffb4
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-10-21 09:01:09 -0500

    Find cart api implimented

--- backend/services/cartService.js
+++ backend/services/cartService.js
@@ -1,5 +1,5 @@
 const Cart = require("../models/cartModel");
-
+const CartItem=require("../models/cartItemModel");
 //Creating cart for user on registration
 const createcart = async (user) => {
   try {
@@ -10,6 +10,18 @@ const createcart = async (user) => {
     throw new Error(err.message);
   }
 };
+
+async function findUserCart(userId) {
+  try {
+    const cart = await Cart.findOne({ userId });  
+    let ccartItems=await CartItem.find({cart:cart._id}).populate("product");
+}
+catch(error)
+{
+
+}
+}
 module.exports = {
   createcart,
 };
+9
