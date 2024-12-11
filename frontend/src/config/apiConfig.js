e2d15603f849edbcd91f1f0e26f04a4318cb392f
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-12-11 12:36:15 -0600

    .

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const LOCALHOST = "https://trend-treasures-backend.onrender.com";
+const LOCALHOST = "http://localhost:8000";
 
 export const API_BASE_URL = LOCALHOST;
 
